05fc2236529960eb5adeb214e1bcc627498f21d8
minductran <minhductran042@gmail.com>
2024-10-06 14:19:08 +0700

    Cai dat pug va database

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("OK");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectDatabase } from "./config/database";
+connectDatabase();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/topics", (req: Request, res: Response) => {
+  res.render("client/pages/topics/index");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+})
