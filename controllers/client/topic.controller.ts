5bd852f4498add87a6686f34fb018e5b5a8792aa
minductran <minhductran042@gmail.com>
2024-10-09 13:38:36 +0700

    Lam trang chu de bai hat va trang danh sach bai hat

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -4,11 +4,14 @@ import Topic from "../../models/topic.model";
 // [GET] /topics
 export const index = async (req: Request, res: Response) => {
 
+
   const topics = await Topic.find({
     deleted: false
   });
 
-  console.log(topics);
-
-  res.render("client/pages/topics/index");
+  
+  res.render("client/pages/topics/index",{
+    pageTitle: "Danh sách chủ đề",
+    topics: topics
+  });
 };
